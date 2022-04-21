4e270a5819a0bd1dbf12f68554ff2c319d322e47
pranav <katkarpranav4@gmail.com>
2022-04-21 15:03:31 +0530

    added route constraints

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Vidly
             routes.MapRoute(
                 "MovieByReleaseData",
                 "movies/released/{year}/{month}",
-                new { controller = "Movies", action = "ByReleaseDate" });
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new {year = @"\d{4}", month = @"\d{2}" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
