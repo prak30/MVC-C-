8f42796ec381abeaefb498a0d035ec9dd9d97662
pranav <katkarpranav4@gmail.com>
2022-04-22 14:20:10 +0530

    migration v2

--- Vidly/Models/MyDbContext.cs
+++ Vidly/Models/MyDbContext.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using Vidly.Models;
 
 namespace Vidly.Models
 {
     public class MyDbContext : DbContext    
     {
-        public MyDbContext()
-        {
-        }
+        public DbSet<Customer> Customers { get; set; }  
     }
 }
